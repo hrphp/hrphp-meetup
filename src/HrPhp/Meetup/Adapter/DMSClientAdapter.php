ab2f6acb3c413e4e64ac3fd52562acdee89638b5
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-02-25 11:14:37 -0500

    Added adapter.

---
+++ src/HrPhp/Meetup/Adapter/DMSClientAdapter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HrPhp\Meetup\Adapter;
+
+use DMS\Service\Meetup\MeetupKeyAuthClient;
+use Zend\Json\Json;
+use HrPhp\Exception\HrPhpException;
+
+class DMSClientAdapter implements AdapterInterface
+{
+    /** @var \DMS\Service\Meetup\MeetupKeyAuthClient */
+    private $client;
+
+    /**
+     * @param \DMS\Service\Meetup\MeetupKeyAuthClient $client
+     */
+    public function __construct(MeetupKeyAuthClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEvents()
+    {
+        try {
+            $events = $this->getClient()->getCommand('GetEvents');
+            return Json::decode($events, Json::TYPE_OBJECT);
+        } catch (\Exception $ex) {
+            throw new HrPhpException('Could not retrieve meetup events');
+        }
+    }
+
+    /**
+     * @return \DMS\Service\Meetup\MeetupKeyAuthClient
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+}
